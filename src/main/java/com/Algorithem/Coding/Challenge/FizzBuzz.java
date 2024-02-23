e0a059c2458b3070107516d041b73aa9391debc1
malachy <malachyomeje@gmail.com>
2024-02-23 11:09:13 +0100

    fizzbuzz commit

--- src/main/java/com/Algorithem/Coding/Challenge/FizzBuzz.java
+++ src/main/java/com/Algorithem/Coding/Challenge/FizzBuzz.java
@@ -23,8 +23,30 @@ public class FizzBuzz {
             else System.out.println(i);
         }
     }
+
+    /**
+     Write a short program that prints
+     For each multiple of 3 print "Fizz"
+     For each multiple of 5 print "Buzz"
+     For each multiple of both 3 and 5 print "FizzBuzz"
+     And print the number if the condition above is not meet.
+     **/
+    public static void print1 (int num){
+        if ((num%3==0) && (num%5==0)) {
+            System.out.println("Fizz-Buzz");
+        }
+       else if (num%3==0){
+            System.out.println("fizz");
+        }
+        else if (num%5==0) {
+            System.out.println("Buzz");
+        }
+        else System.out.println(num);
+    }
+
     public static void main(String[] args) {
-        print(100);
+        print(3);
+        print1(50);
     }
 
   }
